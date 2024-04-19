dd478d1cd94cb62aab7792238d4469ab1b901e90
enfyna <legendraslan@gmail.com>
2024-04-19 23:34:59 +0300

    Oyuncu hareket animasyonu ekle.

--- Oyuncu/Oyuncu.gd
+++ Oyuncu/Oyuncu.gd
@@ -1,12 +1,29 @@
+@tool
 class_name Oyuncu extends CharacterBody2D
+
+@export var Temel : Sprite2D
+@export var Sag : Sprite2D
+@export var Sol : Sprite2D
 
 func _ready():
     pass
 
-func _process(_delta):
-    velocity.x = lerpf(velocity.x, 0.0, 0.2)
-    if Input.is_key_pressed(KEY_RIGHT):
-        velocity.x = 1000
-    elif Input.is_key_pressed(KEY_LEFT):
-        velocity.x = -1000
+var time = 0
+func _process(delta):
+    
+    time += delta * 10
+    Temel.flip_h = velocity.x < 0
+    Sag.flip_h = velocity.x < 0
+    Sol.flip_h = velocity.x < 0
+    Temel.hide()
+    Sag.hide()
+    Sol.hide()
+    if abs(velocity.x) < 10.0:
+        Temel.show()
+    else:
+        if fmod(time, 2.0) > 1.0:
+            Sag.show()
+        else:
+            Sol.show()
+
     move_and_slide()
