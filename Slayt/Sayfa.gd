69793335090f755db241bd39143d985d01b2ac84
enfyna <legendraslan@gmail.com>
2024-05-04 15:55:42 +0300

    Sayfalara sayfa numarası ekle

---
+++ Slayt/Sayfa.gd
@@ -0,0 +1,9 @@
+@tool
+class_name Sayfa extends Control
+
+@onready var lb : Label = $Label
+var sayfa_sayisi : int
+
+func _ready() -> void:
+    var sayfa_no = int(name.split('_')[1]) - 1
+    lb.text = str(sayfa_no )+ "/" + str(sayfa_sayisi)
