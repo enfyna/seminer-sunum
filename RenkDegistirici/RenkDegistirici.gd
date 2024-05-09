d2fbd48a9b71560c8daf1c58bd5914c6c2697d04
enfyna <legendraslan@gmail.com>
2024-05-09 14:10:52 +0300

    Slaytı sonlandır

--- RenkDegistirici/RenkDegistirici.gd
+++ RenkDegistirici/RenkDegistirici.gd
@@ -1,16 +1,16 @@
 @tool
-class_name RenkDegistirici extends Area2D
+class_name Tool_Area2D extends Area2D
 
-func _ready() -> void:
-    pass
+var current = false
 
 func _process(_delta: float) -> void:
-    print(is_connected("body_entered", _on_body_entered))
     if is_connected("body_entered", _on_body_entered):
-        set_process(false)
-    else:
-        connect("body_entered", _on_body_entered)
+        return
+    connect("body_entered", _on_body_entered)
 
 func _on_body_entered(body: Node2D) -> void:
     print(body)
-    body.modulate = Color(randf(), randf(), randf())
+    body.modulate = Color(
+        current, current, current
+    )
+    current = not current
--- Slayt/Slayt.gd
+++ Slayt/Slayt.gd
@@ -12,7 +12,7 @@ func _ready() -> void:
     var sayfa_sayisi = dir.get_files().size()
     for sayfa in dir.get_files():
         var syf : Sayfa = load(klasor + sayfa).instantiate()
-        syf.sayfa_sayisi = sayfa_sayisi
+        syf.sayfa_sayisi = sayfa_sayisi - 1
         syf.sayfa_no = int(sayfa.split("_")[1])
         sayfalar.add_child(syf)
         syf.position.x = syf.sayfa_no - 1
