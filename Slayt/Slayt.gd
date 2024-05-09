cd6eb010411a14d91e22abe50c4c8ab7c72012fc
enfyna <legendraslan@gmail.com>
2024-05-09 19:13:09 +0300

    Export .remap hatasını çöz

--- Slayt/Slayt.gd
+++ Slayt/Slayt.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
     var dir = DirAccess.open(klasor)
     var sayfa_sayisi = dir.get_files().size()
     for sayfa in dir.get_files():
-        var syf : Sayfa = load(klasor + sayfa).instantiate()
+        var syf : Sayfa = load(klasor + sayfa.replace(".remap", "")).instantiate()
         syf.sayfa_sayisi = sayfa_sayisi - 1
         syf.sayfa_no = int(sayfa.split("_")[1])
         sayfalar.add_child(syf)
